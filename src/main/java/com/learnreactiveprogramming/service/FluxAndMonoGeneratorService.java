d3bec37dc2e79a7984a116e06c41962ab6c32087
fredericoffs <37385493+fredericoffs@users.noreply.github.com>
2022-05-30 18:56:49 -0300

    Transform using map() Operator

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -5,7 +5,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-public class FluxAndMonoSchedulersService {
+public class FluxAndMonoGeneratorService {
 
     public Flux<String> namesFlux() {
         return Flux.fromIterable(List.of("alex", "ben", "chloe"))
@@ -17,14 +17,20 @@ public class FluxAndMonoSchedulersService {
                 .log();
     }
 
+    public Flux<String> namesFlux_map() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .map(String::toUpperCase)
+                .log();
+    }
+
     public static void main(String[] args) {
-        FluxAndMonoSchedulersService fluxAndMonoSchedulersService = new FluxAndMonoSchedulersService();
-        fluxAndMonoSchedulersService.namesFlux()
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
                 .subscribe(name -> {
                     System.out.println("Name is: " + name);
                 });
 
-        fluxAndMonoSchedulersService.nameMono()
+        fluxAndMonoGeneratorService.nameMono()
                 .subscribe(name ->{
                     System.out.println("Mono name is: " + name);
                 });
--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -5,17 +5,35 @@ import reactor.test.StepVerifier;
 
 public class FluxAndMonoGeneratorServiceTest {
     
-    FluxAndMonoSchedulersService fluxAndMonoSchedulersService = new FluxAndMonoSchedulersService();
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
     @Test
     void nameFlux() {
-        var namesFlux= fluxAndMonoSchedulersService.namesFlux();
+        var namesFlux= fluxAndMonoGeneratorService.namesFlux();
 
         StepVerifier.create(namesFlux)
                 //.expectNext("alex", "ben", "chloe")
                 //.expectNextCount(3)
                 .expectNext("alex")
                 .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMono() {
+        var nameMono = fluxAndMonoGeneratorService.nameMono();
+
+        StepVerifier.create(nameMono)
+                .expectNext("Other name")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_map() {
+        var namesFlux= fluxAndMonoGeneratorService.namesFlux_map();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("ALEX", "BEN", "CHLOE")
                 .verifyComplete();
     }
 }
