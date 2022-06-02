2e84b4898670c58bd1c2c17c439f59504c155b5e
fredericoffs <37385493+fredericoffs@users.noreply.github.com>
2022-06-02 18:13:28 -0300

    Retrieve all of the MovieInfo

---
+++ src/main/java/com/learnreactiveprogramming/service/MovieReactiveService.java
@@ -0,0 +1,33 @@
+package com.learnreactiveprogramming.service;
+
+import com.learnreactiveprogramming.domain.Movie;
+import com.learnreactiveprogramming.domain.Review;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class MovieReactiveService {
+
+    private MovieInfoService movieInfoService;
+    private ReviewService reviewService;
+
+    public MovieReactiveService(MovieInfoService movieInfoService, ReviewService reviewService) {
+        this.movieInfoService = movieInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public Flux<Movie> getAllMovies(){
+
+        var moviesInfoFlux = movieInfoService.movieInfoFlux();
+        return moviesInfoFlux
+                //reason for using flatmap is because the review service is again going to give you another reactive type.
+                // flatmap - any time in your transformation if you have a function that's returning another reactive type.
+                .flatMap(movieInfo -> {
+                    Mono<List<Review>> reviewsMono = reviewService.retrieveReviewsFlux(movieInfo.getMovieInfoId())
+                            .collectList();
+                    return reviewsMono
+                            .map(reviewsList -> new Movie(movieInfo,reviewsList));
+                }).log();
+    }
+}
