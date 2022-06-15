ad1a5c4283215400fc9c63baaf01034a6a288494
fredericoffs <37385493+fredericoffs@users.noreply.github.com>
2022-06-15 11:52:26 -0300

    Hot Streams - ConnectableFlux using connect()

--- src/test/java/com/learnreactiveprogramming/service/ColdAndHotPublisherTest.java
+++ src/test/java/com/learnreactiveprogramming/service/ColdAndHotPublisherTest.java
@@ -1,7 +1,12 @@
 package com.learnreactiveprogramming.service;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
 public class ColdAndHotPublisherTest {
 
@@ -11,5 +16,20 @@ public class ColdAndHotPublisherTest {
 
         flux.subscribe(i-> System.out.println("Subscriber 1: " + i));
         flux.subscribe(i-> System.out.println("Subscriber 2: " + i));
+    }
+
+    @Test
+    void hotPublisherTest(){
+        var flux = Flux.range(1,10)
+                .delayElements(Duration.ofSeconds(1));
+
+        ConnectableFlux<Integer> connectableFlux = flux.publish();
+        connectableFlux.connect();
+
+        connectableFlux.subscribe(i-> System.out.println("Subscriber 1: " + i));
+        delay(4000);
+
+        connectableFlux.subscribe(i-> System.out.println("Subscriber 2: " + i));
+        delay(10000);
     }
 }
