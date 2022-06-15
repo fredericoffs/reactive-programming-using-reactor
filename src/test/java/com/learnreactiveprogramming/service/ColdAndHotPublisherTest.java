1ad4c60830112377065dc8b438e118818d9a96bf
fredericoffs <37385493+fredericoffs@users.noreply.github.com>
2022-06-15 11:41:28 -0300

    Cold Streams

---
+++ src/test/java/com/learnreactiveprogramming/service/ColdAndHotPublisherTest.java
@@ -0,0 +1,15 @@
+package com.learnreactiveprogramming.service;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+public class ColdAndHotPublisherTest {
+
+    @Test
+    void coldPublisherTest(){
+        var flux = Flux.range(1,10);
+
+        flux.subscribe(i-> System.out.println("Subscriber 1: " + i));
+        flux.subscribe(i-> System.out.println("Subscriber 2: " + i));
+    }
+}
